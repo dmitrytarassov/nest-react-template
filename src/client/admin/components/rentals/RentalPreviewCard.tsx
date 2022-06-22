68002a72ace28e61d1df643b97d384a39811bb9d
Dmitrii Tarasov <dm.tarassov@yandex.ru>
2022-06-22 21:00:28 +0300

    update

--- src/client/admin/components/rentals/RentalPreviewCard.tsx
+++ src/client/admin/components/rentals/RentalPreviewCard.tsx
@@ -6,50 +6,49 @@ import { ThemeProvider } from 'styled-components';
 import PreviewContainer, {
   PreviewPageContainer,
 } from '../previews/PreviewContainer';
+import { ICrudRental } from '@lib/interfaces/ICrudRental';
 
 export default function RentalPreviewCard() {
-  const [record, setRecord] = useState<IRental>({
+  const [record, setRecord] = useState<ICrudRental>({
     id: '',
     name: '',
     url: '',
     icon: '',
-    address: {
-      coordinates: [0, 0],
-      name: '',
-    },
+    address: '',
+    coordinates: '',
     workingTime: '',
     rating: 9,
-    phone: 79998887766,
-    services: [],
+    phone: '79998887766',
     city: 'spb',
-    socials: {},
+    // socials: {},
   });
 
   const update = () => {
-    // @ts-ignore
-    const formData = new FormData(document.forms.rental_form);
-    const name = `${formData.get('name')}`;
-    const phone = formData.get('phone') ? +formData.get('phone') : 0;
-    // @ts-ignore
-    const photos =
-      [
-        // @ts-ignore
-        ...(document.forms.rental_form?.querySelectorAll('.previews img') ||
-          []),
-      ]?.map((e) => e.src) || [];
-
-    const address = `${formData.get('address')}`;
-
-    setRecord((record) => ({
-      ...record,
-      name,
-      phone: phone < 900000000 ? record.phone : phone,
-      icon: photos[0] || '',
-      address: {
-        ...record.address,
-        name: address,
-      },
-    }));
+    // // @ts-ignore
+    // const formData = new FormData(document.forms.rental_form);
+    // const name = `${formData.get('name')}`;
+    // const phone = formData.get('phone') ? +formData.get('phone') : 0;
+    // // @ts-ignore
+    // const photos =
+    //   [
+    //     // @ts-ignore
+    //     ...(document.forms.rental_form?.querySelectorAll('.previews img') ||
+    //       []),
+    //   ]?.map((e) => e.src) || [];
+    //
+    // const address = `${formData.get('address')}`;
+    //
+    // setRecord((record) => ({
+    //   ...record,
+    //   name,
+    //   phone: phone < 900000000 ? record.phone : phone,
+    //   icon: photos[0] || '',
+    //   address: {
+    //     ...record.address,
+    //     name: address,
+    //   },
+    //   coordinates: [],
+    // }));
   };
 
   useEffect(() => {
@@ -66,7 +65,7 @@ export default function RentalPreviewCard() {
     <ThemeProvider theme={theme}>
       <PreviewContainer>
         <PreviewPageContainer>
-          <RentalCard rental={record} active={false} />
+          {/*<RentalCard rental={record} active={false} />*/}
         </PreviewPageContainer>
       </PreviewContainer>
     </ThemeProvider>
